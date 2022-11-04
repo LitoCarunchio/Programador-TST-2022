dc2da3f472c34244da761742aa4d0d7327ebede8
LitoCarunchio <carlos.carunchio@mi.unc.edu.ar>
2022-11-04 02:49:21 -0300

    agrego menu y clases con entidad, tambien ordeno repo

---
+++ BD inmobiliaria/Clases/claseestado.py
@@ -0,0 +1,43 @@
+from os import system
+class estado:
+    Nombre = ""
+
+    def __init__(self, nombre=''):
+        self.Nombre = nombre
+
+    def getNombre(self):
+        return self.Nombre
+
+    def setNombre(self, nombre):
+        self.Nombre = nombre
+
+    def __str__(self):
+        return str(self.Nombre)
+
+    def definirEstado(self):  # funcion para definir id de estado de propiedad
+        correcto = False
+        while not correcto:
+            try:
+                print("""Indique el estado de la propiedad de la siguiente lista:
+                    1. Disponible
+                    2. Alquilada
+                    3. Vendida
+                    4. Sin definir """)
+
+                op = int(input("Ingrese una opcion numérica: "))
+                if op in range(1, 5):
+                    correcto = True
+                    return op
+                else:
+                    print()
+                    print("Error al ingresar la opción estado")
+                    continua = input(' Presione una tecla para continuar...')
+                    system("cls")
+
+            except Exception as e:
+                print()
+                print("Error al ingresar la opción", e)
+                continua = input(' Presione una tecla para continuar...')
+                system("cls")
+if __name__ == '__main__':
+    print("Soy la Clase Estado")
---
+++ BD inmobiliaria/Clases/claseoperatoria.py
@@ -0,0 +1,46 @@
+
+from os import system
+class operatoria:
+    NombreOp = ""
+
+    def __init__(self, nombre=''):
+        self.NombreOp = nombre
+
+    def getNombreOp(self):
+        return self.NombreOp
+
+    def setNombreOp(self, nombre):
+        self.NombreOp = nombre
+
+    def __str__(self):
+        return str(self.NombreOp)
+    
+    def definirOperatoria(self):  # Esta funcion es para definir id de operatoria de propiedad
+        correcto = False
+        while not correcto:
+            try:
+                print("""Indique la operatoria de la propiedad de la siguiente lista:
+                        1. Alquiler
+                        2. Venta
+                        3. Sin Definir
+                        """)
+                op = int(input("Ingrese una opcion numerica: "))
+                if op in range(1, 4):
+                    correcto = True
+                    return op
+                else:
+                    print()
+                    print("Error al ingresar la opción operatoria")
+                    continua = input(' Presione una tecla para continuar...')
+                    system("cls")
+
+            except Exception as e:
+                print()
+                print("Error al ingresar la opcion", e)
+                continua = input(' Presione una tecla para continuar...')
+                system("cls")
+
+
+        
+if __name__ == '__main__':
+    print("Soy la Clase Operatoria")
