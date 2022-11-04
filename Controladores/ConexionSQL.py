99d2ae7d9213d69eb553041ce80940230bc678b0
Gise Romero <98425890+GiseRomero@users.noreply.github.com>
2022-11-04 02:12:45 -0300

    Update ConexionSQL.py

--- Controladores/ConexionSQL.py
+++ Controladores/ConexionSQL.py
@@ -1,26 +1,52 @@
-from sqlite3 import Cursor
 import mysql.connector
+import time
 
-class conexionBD ():
-
-    def __init__(self) -> None:
+class conexion: # Clase conexion
+    def __init__(self):
         try:
-            self.conexion = mysql.connector.connect (
-            host = 'localhost',
-            port = 3306,
-            user = 'root',
-            password = 'root',
-            db  = 'Inmobiliaria'
-            )
-            if self.conexion.is_connected ():
-                print("¡Conexión Exitosa!")
-        except mysql.connector.Error as DescripcionError:
-            print ("¡Conexión Fallida!", DescripcionError)
-        finally:
-            if self.conexion.is_connected():
-               self.conexion.close()
-               print ("Conexión Cerrada")
-               
-conexion = conexionBD ()
+            self.conexion = mysql.connector.connect(
+                host='localhost',
+                port=3306,
+                user='root',   
+                password='',   
+                db=''  
+                )           
+        except mysql.connector.Error as err:
+            print('Error al conectar a la Base de Datos')
+            print(err)
 
-print (conexion)
+    def getConection(self):
+        return self.conexion
+
+    def setConection(self, conexion):
+        self.conexion = conexion
+
+    def __str__(self):
+        return "Conexion a la Base de Datos"
+
+    def conectar(self): # Conectar  a la base de datos
+        try:
+            self.conexion.connect()
+            if self.conexion.is_connected():
+                print('Conexión exitosa ' +
+                      time.strftime("%x") + '  ' + time.strftime("%X"))
+        except mysql.connector.Error as err:
+            print('Error al conectar a la Base de Datos')
+            print(err)
+
+    def desconectar(self): #Desconectamosla base de datos   
+        try:
+            if self.conexion.is_connected():
+                self.conexion.close()
+                print('Conexión cerrada ' +
+                      time.strftime("%x") + '  ' + time.strftime("%X"))
+        except mysql.connector.Error as err:
+            print('Error al conectar a la Base de Datos')
+            print(err)
+
+    def conectado(self): # Verificamos si la conexion esta activa
+        return self.conexion.is_connected()
+
+
+if __name__ == '__main__':
+    print('Soy la conexión a la Base de Datos')
